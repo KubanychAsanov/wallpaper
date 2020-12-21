9d86571e6c21734741c1027ff2e935d7320cdd6e
Kuba <kuba.asanov4@gmail.com>
2020-12-21 22:40:21 +0600

    added styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageGrid from './body/ImageGrid';
+import Modal from './body/Modal';
 import Title from './body/Title';
 import Upload from './body/Upload';
 
 function App() {
+  const [selectedImg, setselectedImg] = useState(null);
+  
   return (
     <div className="App">
       <Title/>
       <Upload/>
-      <ImageGrid/>
+      <ImageGrid setselectedImg = {setselectedImg}/>
+      { selectedImg && <Modal selectedImg = {selectedImg} setselectedImg = {setselectedImg}/>}
     </div>
   );
 }
--- src/body/ImageGrid.js
+++ src/body/ImageGrid.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import useFirestore from '../firebase/useFirestore';
 
-const ImageGrid = () => {
+const ImageGrid = ({setselectedImg}) => {
     const {docs} = useFirestore('images');
     console.log(docs);
     return(
         <div className="img-grid">
             {docs && docs.map(doc =>(
-                <div className="img-wrap" key={doc.id}>
+                <div className="img-wrap" key={doc.id} onClick={() => setselectedImg(doc.url)}>
                     <img src={doc.url} alt="uploaded pic"></img>
                 </div>
             ))}
