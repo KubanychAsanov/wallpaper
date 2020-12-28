626001b3be92d5bc5d3b0a4a0667fbc7a5f1e53e
Kuba <kuba.asanov4@gmail.com>
2020-12-28 20:41:18 +0600

    added fields(icons) "like" instagram

--- src/body/ImageGrid.js
+++ src/body/ImageGrid.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import useFirestore from '../firebase/useFirestore';
+import { faHeart as fasFaHeart} from "@fortawesome/free-solid-svg-icons";
+import { faHeart as farFaHeart} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ImageGrid = ({setselectedImg}) => {
+    const [heart, setHeart] = useState(farFaHeart);
     const {docs} = useFirestore('images');
     console.log(docs);
+
+    const changeColor = (e) => {
+        if (heart==farFaHeart) {
+            setHeart(fasFaHeart);
+        }else{
+            setHeart(farFaHeart)
+        }
+    }
+
     return(
         <div className="img-grid">
             {docs && docs.map(doc =>(
-                <div className="img-wrap" key={doc.id} onClick={() => setselectedImg(doc.url)}>
-                    <img src={doc.url} alt="uploaded pic"></img>
+                <div className="img-wrap" key={doc.id}>
+                    <img onClick={() => setselectedImg(doc.url)} src={doc.url} alt="uploaded pic"></img>
+                    <br></br>
+                    <div>
+                        <FontAwesomeIcon
+                            className="like" 
+                            onClick={() => {
+                                changeColor();
+                            }}
+                            icon={heart} style={{color: 'red'}}/>
+                    </div>
                 </div>
             ))}
         </div>
