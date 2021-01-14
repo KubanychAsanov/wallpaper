4dd8bdd82de9c7cb187a0861a9ac48937c75a199
Kuba <kuba.asanov4@gmail.com>
2021-01-14 13:30:03 +0600

    some changes

--- src/body/ImageGrid.js
+++ src/body/ImageGrid.js
@@ -1,21 +1,21 @@
 import React,{ useState} from 'react';
 import useFirestore from '../firebase/useFirestore';
-import { faHeart as fasFaHeart} from "@fortawesome/free-solid-svg-icons";
-import { faHeart as farFaHeart} from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart as fasFaHeart} from "@fortawesome/free-solid-svg-icons";
+// import { faHeart as farFaHeart} from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ImageGrid = ({setselectedImg}) => {
-    const [heart, setHeart] = useState(farFaHeart);
+//     const [heart, setHeart] = useState(farFaHeart);
     const {docs} = useFirestore('images');
     console.log(docs);
 
-    const changeColor = (e) => {
-        if (heart==farFaHeart) {
-            setHeart(fasFaHeart);
-        }else{
-            setHeart(farFaHeart)
-        }
-    }
+    // const changeColor = (e) => {
+    //     if (heart==farFaHeart) {
+    //         setHeart(fasFaHeart);
+    //     }else{
+    //         setHeart(farFaHeart)
+    //     }
+    // }
 
     return(
         <div className="img-grid">
@@ -23,14 +23,14 @@ const ImageGrid = ({setselectedImg}) => {
                 <div className="img-wrap" key={doc.id}>
                     <img onClick={() => setselectedImg(doc.url)} src={doc.url} alt="uploaded pic"></img>
                     <br></br>
-                    <div>
+                    {/* <div>
                         <FontAwesomeIcon
                             className="like" 
                             onClick={() => {
                                 changeColor();
                             }}
                             icon={heart} style={{color: 'red'}}/>
-                    </div>
+                    </div> */}
                 </div>
             ))}
         </div>
